e10003daad1f914e29d425fffa005751e9199230
frameless Media <office@frameless.at>
2026-01-15 11:52:50 +0100

    Bumped version to 1.1.1

--- ProcessDataMigrator.module.php
+++ ProcessDataMigrator.module.php
@@ -20,8 +20,8 @@ require_once(__DIR__ . '/classes/Logger.php');
  *
  * Migrate external data (SQL, CSV, JSON, XML) into ProcessWire
  *
- * @author ProcessWire
- * @version 1.1.0
+ * @author frameless Media
+ * @version 1.1.1
  */
 class ProcessDataMigrator extends Process implements Module, ConfigurableModule {
 
@@ -32,7 +32,7 @@ class ProcessDataMigrator extends Process implements Module, ConfigurableModule 
         return [
             'title' => 'Data Migrator',
             'summary' => 'Migrate external data (SQL, CSV, JSON, XML) into ProcessWire',
-            'version' => '1.1.0',
+            'version' => '1.1.1',
             'author' => 'ProcessWire',
             'icon' => 'exchange',
             'permission' => 'data-migrate',
